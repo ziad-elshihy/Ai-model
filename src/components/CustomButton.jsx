84a62b721d4d04e3325fc1a1fb326094a564668a
ziad-elshihy <elshihyziad@gmail.com>
2023-08-26 21:31:06 +0300

    edit

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -9,7 +9,7 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
       if (type === 'filled') {
          return {
             backgroundColor: snap.color,
-            color: "#fff"
+            color: "#000"
          }
       }
    }
